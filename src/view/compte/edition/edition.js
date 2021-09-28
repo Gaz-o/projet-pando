1512d9e9e3a375bee7f00da9899939bee68b4e23
Virginie Fiorucci <vfiorucci@live.fr>
2021-09-28 23:02:48 +0200

    Edition du profil - début des modifications

--- src/view/compte/edition/edition.js
+++ src/view/compte/edition/edition.js
@@ -1,10 +1,20 @@
 import "../compte.css";
+import { getCurrentUserProfile } from "../../../lib/social-network-library-master";
+import { useState } from "react";
 
 function Edition () {
 
+    /* Variable d'état */
+    const [profile, SetProfile] = useState({firstname:"", lastname:"", email:"", _id:""});
+
+    const handleClick = async () => {
+        let currentUserProfile = await getCurrentUserProfile();
+    }
+
     return (
         <div>
-            <h1>Edition</h1>
+            <h3>Edition</h3>
+            <button onClick={handleClick}>Afficher</button>
         </div>
     )
 }
