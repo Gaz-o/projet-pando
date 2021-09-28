2e4d633113545dbce5154329f75800912d638a1d
Robuchon Hugues <hugues.robuchon@gmail.com>
2021-09-28 14:37:12 +0200

    rajout du crea dans home

--- src/view/compte/connexion.js
+++ src/view/compte/connexion.js
@@ -1,10 +1,12 @@
 import "./compte.css";
+import Crea from "./crea/crea";
 
 function Home () {
-    
+
     return (
         <div>
             <h1>Home</h1>
+            <Crea/>
         </div>
     )
 };
