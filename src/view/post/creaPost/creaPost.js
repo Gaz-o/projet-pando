2eeb0d6e3655e79012be3896bd6da33db4f0d8af
Guerrier Marc-Aurele <gma06000@gmail.com>
2021-09-28 20:24:05 +0200

    ébauche de l'éditeur de post

--- src/view/post/creaPost/creaPost.js
+++ src/view/post/creaPost/creaPost.js
@@ -1,36 +1,55 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container,Form, FormControl, InputGroup ,Button} from 'react-bootstrap';
 import "../post.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function CreatePost() {
 
-    const [status, setStatus] = useState({ title: "", content: "", etat:"Posted" });
-    //const [content, setContent] = useState("");
-    const [emptyField, setemptyField] = useState("");
+    const [status, setStatus] = useState({ content:"", title:"", etat:"Posted" });
+    const [emptyField, setMessage] = useState("");
+    const [showPosts, setShowposts] = useState("");
     const [feed, setFeed] = useState([]);
 
-    /*const getTitle = (e) => {
-        setStatus(...title, e.target.value);
+    const getTitle = (e) => {
+        setStatus({...status, title: e.target.value});
+        console.log("title on change:" +e.target.value);
     }
     const getContent = (e) => {
-        setStatus(...content, e.target.value);
-    }*/
+        setStatus({...status, content: e.target.value});
+        console.log("content on change :" +e.target.value);
+    }
 
     const post = (e) => {
 
-        /*if (title !== "" && content !== "") {
-            setemptyField("Tous les champs sont requis");
+        if (status.title === "" && status.content === "") {
+            setMessage("Tous les champs sont requis");
         }else{
             setFeed([...feed, status]);
-                console.log(feed);
-        }*/
+            setStatus({...status.etat, etat: "Posted"});
+            console.log(status.content+"-"+status.title);
+            setShowposts(status.content+"-"+status.title);
+        }
+
     }
+
+    
+    useEffect (() => 
+        console.log(status), [status]
+    );
+    useEffect (() => 
+        console.log(feed), [feed]
+    );
+
+    useEffect (() => 
+        console.log(showPosts), [showPosts]
+    );
+
+    
     return (
 
         <div>
 
-            <h1>Publier</h1>
+            <h6>Publier sur Pando</h6>
 
                 <Container>
 
@@ -40,15 +59,17 @@ function CreatePost() {
 
                         <InputGroup className="mb-3">
 
-                            <FormControl id="postTitle" type="text"  placeholder="Ajouter un titre :" aria-label="task" aria-describedby="basic-addon"/>
-
+                            <FormControl id="postTitle" type="text" onChange={getTitle} placeholder="Ajouter un titre :" aria-label="task" aria-describedby="basic-addon"/>
+                            
                         </InputGroup>
 
-                        <FormControl as="textarea" id="postContent" type="text"  placeholder="Votre contenu :" aria-label="task" aria-describedby="basic-addon"/>
+                        <FormControl as="textarea" id="postContent" type="text" onChange={getContent} placeholder="Votre contenu :" aria-label="task" aria-describedby="basic-addon"/><br/><br/>
 
                         <Button variant="outline-success" onClick={post}>Publier</Button>
 
                     </Form>
+
+                    <p>{showPosts}</p>
 
                 </Container>
 
