c9ca13c369deb1fc6322b2ece9012d87e014e4df
Robuchon Hugues <hugues.robuchon@gmail.com>
2021-09-28 22:42:02 +0200

    redirection si pas co

--- src/layout/routage/routage.js
+++ src/layout/routage/routage.js
@@ -1,5 +1,5 @@
 import "./routage.css";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Compte from "../../view/compte/compte.js";
 import Edition from "../../view/compte/edition/edition.js"
 import Postcrea from "../../view/post/creaPost/creaPost.js";
@@ -7,10 +7,11 @@ import Home from "../../view/compte/connexion.js";
 import MaPage from "../../view/maPage/maPage.js";
 import Nourrir from "../../view/feed/feed.js";
 import Header from "../header/header";
-
-
+import { isUserLoggedIn } from "../../lib/social-network-library-master";
 
 function Routage () {
+  
+  console.log(isUserLoggedIn());
 
   return (
     <Router>
@@ -18,17 +19,16 @@ function Routage () {
         <Header/>
         <Switch>
           <Route path="/compte">
-            <Compte />
-            <Edition/>
+            {isUserLoggedIn() === true ? (<Edition/>,<Compte />) : <Redirect to="/" />}
           </Route>
           <Route path="/feed">
             <Nourrir />
           </Route>
           <Route path="/creaPost">
-            <Postcrea />
+            {isUserLoggedIn() === true ? <Postcrea /> : <Redirect to="/" />}
           </Route>
           <Route path="/maPage">
-            <MaPage />
+            {isUserLoggedIn() === true ? <MaPage /> : <Redirect to="/" />}
           </Route>
           <Route path="/">
             <Home />
