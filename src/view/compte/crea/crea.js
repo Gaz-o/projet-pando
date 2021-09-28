1fd2fabd2a869fc6c07f3b9cd30d9da555059226
Virginie Fiorucci <vfiorucci@live.fr>
2021-09-28 15:11:08 +0200

    Modification de la page création de compte

--- src/view/compte/crea/crea.js
+++ src/view/compte/crea/crea.js
@@ -1,40 +1,47 @@
 import "../compte.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function Crea() {
 
-    /* Variable d'états - Coordonnées */
+    /* Variable d'états - Informations */
     const [firstName, SetFirstName] = useState("");
     const [lastName, SetLastName] = useState("");
     const [email, SetEmail] = useState("");
-    const [birthday, SetBirthday] = useState();
+    const [password, SetPassword] = useState("");
 
-    /* Fonctions de mise à jour - Coordonnées */
+    /* Fonctions de mise à jour - Informations */
     const handleFirstNameInput = (e) => {
         SetFirstName(e.target.value);
     }
     
     const handleLastNameInput = (e) => {
-        setLastName(e.target.value);
+        SetLastName(e.target.value);
     }
 
     const handleEmailInput = (e) => {
-        setEmail(e.target.value);
+        SetEmail(e.target.value);
+    }
+
+    const handlePasswordInput = (e) => {
+        SetPassword(e.target.value);
     }
 
     /* Evénénement clic sur le bouton */
     const handleClick = () => {
-        console.log("Suivant");
+        console.log(firstName, lastName, email, password);
     }
 
   return (
     <div>
-      <h1>Créez votre compte</h1>
+      <h3>Créez votre compte</h3>
       <input onChange={handleFirstNameInput} type="text" id="firstNameInput" placeholder="Prénom"></input>
+      <br />
       <input onChange={handleLastNameInput} type="text" id="lastNameInput" placeholder="Nom de famille"></input>
+      <br />
       <input onChange={handleEmailInput} type="email" id="emailInput" placeholder="Adresse e-mail"></input>
-      <label for="birthdayInput">Date de naissance:</label>
-      <input onChange={handleBirthdayInput} type="date" id="birthdayInput" name="birthdayInput"></input>
+      <br />
+      <input onChange={handlePasswordInput} type="password" id="emailInput" placeholder="Mot de passe"></input>
+      <br />
       <button onClick={handleClick}>Suivant</button>
     </div>
   );
