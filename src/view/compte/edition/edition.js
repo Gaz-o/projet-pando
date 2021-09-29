2f66108eea8d2f03fe668b2937322ca7e4797c38
Virginie Fiorucci <vfiorucci@live.fr>
2021-09-29 11:25:34 +0200

    Le commit de la galère

--- src/view/compte/edition/edition.js
+++ src/view/compte/edition/edition.js
@@ -1,16 +1,31 @@
 import "../compte.css";
-import { getCurrentUserProfile, isUserLoggedIn } from "../../../lib/social-network-library-master";
-import { useState } from "react";
+import { getCurrentUserProfile, isUserLoggedIn, updateCurrentUserProfile } from "../../../lib/social-network-library-master";
+import { useState, useEffect } from "react";
 import { Redirect } from 'react-router-dom';
 
 function Edition () {
 
-    /* Variable d'état */
-    const [profile, SetProfile] = useState({firstname:"", lastname:"", email:"", _id:""});
+    /* Variables d'état */
+    /* const [profile, setProfile] = useState({firstname:"", lastname:"", email:"", _id:""}); */
+    let userProfile = "";
 
+    /* Récupération des infos du profil connecté */
     const handleClick = async () => {
-        let currentUserProfile = await getCurrentUserProfile();
+        userProfile = await getCurrentUserProfile();
+        console.log(userProfile, 1);
     }
+    
+   /*  useEffect(() => {
+        if(userProfile !== "") {
+            setProfile({...profile, 
+            firstname: userProfile.firstname, 
+            lastname: userProfile.lastname, 
+            email: userProfile.email, 
+            _id: userProfile._id
+            }, console.log(profile, 2));
+        }
+    })
+    */
 
     if(isUserLoggedIn() !== true) {
         return <Redirect to="/" />
@@ -18,8 +33,13 @@ function Edition () {
 
     return (
         <div>
-            <h3>Edition</h3>
-            <button onClick={handleClick}>Afficher</button>
+            <h3>Mon profil</h3>
+            <p>Prénom: <span>{userProfile.firstname}</span></p>
+            <p>Nom: <span></span></p>
+            <p>Âge: <span></span></p>
+            <p>Email: <span></span></p>
+            <p>Poste: <span></span></p>
+            <button onClick={handleClick}>Test</button>
         </div>
     )
 }
