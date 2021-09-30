dfd60da3703869b672bab0ca52ad6b81a063a4ad
Robuchon Hugues <hugues.robuchon@gmail.com>
2021-09-30 15:34:27 +0200

    header css

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, Nav, Button} from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import "./header.css";
 import { Link } from 'react-router-dom';
 
@@ -9,34 +9,19 @@ function Header() {
     return (
         <div>
             <Navbar bg="success" expand="lg" className="border-bottom">
-
                 <Container>
-
-                        <Navbar.Brand href="/home">Pando - Réseau social</Navbar.Brand>
-
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                                
-                                    <Navbar.Collapse id="basic-navbar-nav">
-
-                                    <Nav>
-                                            <Link className="nav-link" to="/compte"><Button variant="success" className="button">Compte</Button></Link>
-                                            <Link className="nav-link" to="/feed"><Button variant="success" className="button">L'arbre foret</Button></Link>
-                                            
-                                    </Nav>
-
-                                    <Nav>
-
-                                            <Link className="nav-link" to="/creaPost"><Button variant="success" className="button">Ma feuille</Button></Link>
-                                            <Link className="nav-link" to="/maPage"><Button variant="success" className="button">Ma branche</Button></Link>
-
-                                    </Nav>
-
-                            </Navbar.Collapse>
-
+                    <h3 href="/home">Pando - Réseau social</h3>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="">
+                        <Nav>
+                            <Link className="nav-link" to="/compte"><Button variant="success" className="button">Compte</Button></Link>
+                            <Link className="nav-link" to="/feed"><Button variant="success" className="button">L'arbre foret</Button></Link>
+                            <Link className="nav-link" to="/creaPost"><Button variant="success" className="button">Ma feuille</Button></Link>
+                            <Link className="nav-link" to="/maPage"><Button variant="success" className="button">Ma branche</Button></Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
-            
             </Navbar>
-
         </div>
     );
 }
