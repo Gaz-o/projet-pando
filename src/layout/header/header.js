de4d9b494076f0ab06e49b9aa8fec1db2cb9ebee
Guerrier Marc-Aurele <gma06000@gmail.com>
2021-09-30 15:15:45 +0200

    Overflow corrigé

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,46 +1,46 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, Nav, Button} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import "./header.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Header() {
+  /* Implémentation des links de react-router-dom */
+  return (
+    <div>
+      <Navbar bg="success" expand="lg" className="border-bottom">
+        <Container>
+          <Navbar.Brand href="/home">Pando - Réseau social</Navbar.Brand>
 
-    /* Implémentation des links de react-router-dom */
-    return (
-        <div>
-            <Navbar bg="success" expand="lg" className="border-bottom">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-                <Container>
-
-                        <Navbar.Brand href="/home">Pando - Réseau social</Navbar.Brand>
-
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                                
-                                    <Navbar.Collapse id="basic-navbar-nav">
-
-                                    <Nav>
-                                            <Link className="nav-link" to="/compte"><Button variant="success" className="button">Compte</Button></Link>
-                                            <Link className="nav-link" to="/feed"><Button variant="success" className="button">L'arbre foret</Button></Link>
-                                            
-                                    </Nav>
-
-                                    <Nav>
-
-                                            <Link className="nav-link" to="/creaPost"><Button variant="success" className="button">Ma feuille</Button></Link>
-                                            <Link className="nav-link" to="/maPage"><Button variant="success" className="button">Ma branche</Button></Link>
-
-                                    </Nav>
-
-                            </Navbar.Collapse>
-
-                </Container>
-            
-            </Navbar>
-
-        </div>
-    );
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Link className="nav-link" to="/compte">
+                <Button variant="success" className="button">
+                  Compte
+                </Button>
+              </Link>
+              <Link className="nav-link" to="/feed">
+                <Button variant="success" className="button">
+                  L'arbre foret
+                </Button>
+              </Link>
+              <Link className="nav-link" to="/creaPost">
+                <Button variant="success" className="button">
+                  Ma feuille
+                </Button>
+              </Link>
+              <Link className="nav-link" to="/maPage">
+                <Button variant="success" className="button">
+                  Ma branche
+                </Button>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
-
-
 
 export default Header;
